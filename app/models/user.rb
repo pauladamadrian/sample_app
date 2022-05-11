646883cc4bec8b7ee5fc186847548e3783c653db
Paul Adam <adrian.adam@takeofflabs.com>
2022-05-11 08:36:24 +0300

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   # before_save { self.email = email.downcase }  # self is optional on the right-hand side
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }  # validates(:name, presence: true)
@@ -9,9 +10,31 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::ENGINE.cost
+  #* Returns the hash digest of the given string.
+  def User.digest(string)   # OR def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  #* Returns a random token.
+  def User.new_token
+    SecureRandom::urlsafe_base64      # Ruby implicitly returns the value of the last expression in a function
+  end
+
+  #* Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #* Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)    # remember_digest is created by Active Record based on the database column
+  end
+
+  #* Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -29,6 +29,8 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    #* Simulate a user clicking logout in a secon window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
@@ -44,5 +46,20 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies[:remember_token]
+    assert_equal cookies[:remember_token] , assigns(:user).remember_token # we can acces user's remember token, which is virtual
+                              # in controller, because we declared user as an instance variable there (@)
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
   end
 end

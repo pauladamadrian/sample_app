80b7de6e4b4bb959e818e9ef357a859acba69e66
Paul Adam <adrian.adam@takeofflabs.com>
2022-05-13 11:15:36 +0300

    Revert last 2 changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,9 +68,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => 587,
+    :port => '587',
     :authentication => :plain,
-    :user_name => 'apikey',
+    :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
